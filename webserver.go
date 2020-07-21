58f9a5ef8c6944ce869f3f3b2deaabc18144f92d
Fixer <fixer@email.com>
2020-07-21 15:11:02 +0300

    Fixed webserver.go

--- webserver.go
+++ webserver.go
@@ -2,14 +2,13 @@ package main
 
 import ( 
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, this is a simple web server, it only responds with this message")
+		fmt.Fprintf(w, "Hello, this is a simple web server, it only responds with this message\n")
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
