4bc98c9707905750293e653036517f3b69670adb
beror <34488024+beror@users.noreply.github.com>
2020-10-19 13:37:50 +0300

    Added POST, PUT, DELETE requests

--- REST.dart
+++ REST.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
+String address = "http://192.168.0.103:8085/PLanguages";
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -38,12 +40,60 @@ class MyHomePage extends StatelessWidget {
               margin: EdgeInsets.fromLTRB(10, 4, 10, 10),
               padding: EdgeInsets.all(30),
               decoration: BoxDecoration(
-                color: Colors.amber,
+                color: Colors.green,
                 borderRadius: BorderRadius.circular(50)
               ),
               child: Text(
                 "Get",
                 style: TextStyle(fontSize: 30))
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => PostFormRoute()));
+            },
+            child: Container(
+                margin: EdgeInsets.fromLTRB(10, 4, 10, 10),
+                padding: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.circular(50)
+                ),
+                child: Text(
+                    "Post",
+                    style: TextStyle(fontSize: 30))
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => PutFormRoute()));
+            },
+            child: Container(
+                margin: EdgeInsets.fromLTRB(10, 4, 10, 10),
+                padding: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(50)
+                ),
+                child: Text(
+                    "Put",
+                    style: TextStyle(fontSize: 30))
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => DeleteFormRoute()));
+            },
+            child: Container(
+                margin: EdgeInsets.fromLTRB(10, 4, 10, 10),
+                padding: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(50)
+                ),
+                child: Text(
+                    "Delete",
+                    style: TextStyle(fontSize: 30))
             ),
           )
         ],
@@ -55,30 +105,22 @@ class MyHomePage extends StatelessWidget {
 class GetRoute extends StatelessWidget {
   int requestItemCount;
   var jsonDecodedBody;
-  Future<Response> getGetFuture() async {
-    var response = await get('http://192.168.0.105:8085/PLanguages');
+
+  Future<Response> getGet() async {
+    var response = await get(address);
     jsonDecodedBody = jsonDecode(response.body);
     requestItemCount = jsonDecodedBody.length;
     return response;
-    /*List<dynamic> responseBody = jsonDecode(response.body);
-    responseBody.forEach((element) {
-      print(element.forEach((key, value) {
-        print("$element:");
-        print("Key: $key");
-        print("Value: $value");
-        print("Key type: ${key.runtimeType}");
-        print("Value type: ${value.runtimeType}");
-      }));
-    });*/
   }
+
   @override
   Widget build(BuildContext context) => FutureBuilder(
-    future: getGetFuture(),
+    future: getGet(),
     builder: (context, snapshot) {
       if(snapshot.hasData) {
         return Scaffold(
             appBar: AppBar(
-                title: Text("Get")
+                title: Text("Get all entries")
             ),
             body: ListView.builder(
                 itemBuilder: (_, i) {
@@ -110,12 +152,197 @@ class GetRoute extends StatelessWidget {
                     ],
                   );
                 },
-              itemCount: jsonDecodedBody.length,
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 0)
+              itemCount: jsonDecodedBody.length
             )
         );
       }
-      else return Text("Loading");
+      else return Scaffold(appBar: AppBar(title: null), body: Center(child: CircularProgressIndicator()));
     }
   );
+}
+
+class PostFormRoute extends StatefulWidget {
+  @override
+  createState() => PostFormRouteState();
+}
+
+class PostFormRouteState extends State<PostFormRoute> {
+  final postFormKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+        AppBar(
+          title: Text("Post an entry")
+        ),
+      body: Column(
+          children: [
+            Form(
+              key: postFormKey,
+              child: Padding(
+                  child: TextFormField(
+                    onSaved: (value) async {
+                    print(value);
+                    String postBody = "{"
+                        "\"name\": \"$value\""
+                        "}";
+                    var response = await post(address, body: postBody);
+                    print(jsonDecode(response.body));
+                    },
+                  decoration: InputDecoration(hintText: "Language name")
+                ),
+                padding: EdgeInsets.fromLTRB(35, 10, 35, 0)
+              )
+            ),
+            Padding(
+              child: RaisedButton.icon(
+                icon: Icon(Icons.add, size: 50),
+                label: Text("Add", style: TextStyle(fontSize: 40)),
+                onPressed: () {
+                  postFormKey.currentState.save();
+                },
+                color: Colors.white12,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                padding: EdgeInsets.fromLTRB(25, 20, 25, 20)
+              ),
+              padding: EdgeInsets.all(20)
+            )
+          ]
+      )
+      );
+  }
+}
+
+class PutFormRoute extends StatefulWidget {
+  @override
+  createState() => PutFormRouteState();
+}
+
+class PutFormRouteState extends State<PutFormRoute> {
+  final putFormKeyIDToEdit = GlobalKey<FormState>();
+  final putFormKeyID = GlobalKey<FormState>();
+  final putFormKeyName = GlobalKey<FormState>();
+  var jsonDecodedBody;
+
+  var idToEdit;
+  var idToChangeTo;
+  var nameToChangeTo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar:
+        AppBar(
+            title: Text("Put an entry")
+        ),
+        body: Column(
+            children: [
+              Form(
+                  key: putFormKeyIDToEdit,
+                  child: Padding(
+                      child: TextFormField(
+                          onSaved: (value) async {
+                          idToEdit = value;
+                          },
+                          decoration: InputDecoration(hintText: "Current ID to be changed")
+                      ),
+                      padding: EdgeInsets.fromLTRB(35, 10, 35, 20)
+                  )
+              ),
+              Form(
+                  key: putFormKeyID,
+                  child: Padding(
+                      child: TextFormField(
+                        onSaved: (value) async {
+                        idToChangeTo = value;
+                        },
+                        decoration: InputDecoration(hintText: "ID to be changed to"),
+                      ),
+                      padding: EdgeInsets.fromLTRB(35, 10, 35, 0)
+                  )
+              ),
+              Form(
+                  key: putFormKeyName,
+                  child: Padding(
+                      child: TextFormField(
+                        onSaved: (value) async {
+                          nameToChangeTo = value;
+                          },
+                        decoration: InputDecoration(hintText: "Name to be changed to"),
+                      ),
+                      padding: EdgeInsets.fromLTRB(35, 10, 35, 0)
+                  )
+              ),
+              Padding(
+                  child: RaisedButton.icon(
+                      icon: Icon(Icons.edit, size: 40),
+                      label: Text("Edit", style: TextStyle(fontSize: 40)),
+                      onPressed: () async {
+                        putFormKeyIDToEdit.currentState.save();
+                        putFormKeyID.currentState.save();
+                        putFormKeyName.currentState.save();
+
+                        String putBody = "{"
+                            "\"id\": $idToChangeTo,"
+                            "\"name\": \"$nameToChangeTo\""
+                            "}";
+                        print(putBody);
+                        var response = await put(address + "/" + idToEdit, body: putBody);
+                        print(response.body);
+                      },
+                      color: Colors.white12,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                      padding: EdgeInsets.fromLTRB(25, 20, 25, 20)
+                  ),
+                  padding: EdgeInsets.all(20)
+              )
+            ]
+        )
+    );
+  }
+}
+
+class DeleteFormRoute extends StatefulWidget {
+  @override
+  createState() => DeleteFormRouteState();
+}
+
+class DeleteFormRouteState extends State<DeleteFormRoute> {
+  var deleteFormKey = GlobalKey<FormState>();
+
+  @override
+  build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Delete an entry")),
+      body: Column(
+        children: [
+          Form(
+            key: deleteFormKey,
+            child: Padding(
+              child: TextFormField(
+                  onSaved: (value) async {
+                    var response = await delete(address + "/" + value);
+                    print(response);
+                    },
+                  decoration: InputDecoration(hintText: "Language ID to delete")
+              ),
+                padding: EdgeInsets.fromLTRB(35, 10, 35, 0)
+            )
+          ),
+          Padding(
+              child: RaisedButton.icon(
+                  icon: Icon(Icons.delete, size: 40),
+                  label: Text("Delete", style: TextStyle(fontSize: 40)),
+              onPressed: () => deleteFormKey.currentState.save(),
+              color: Colors.white12,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+              padding: EdgeInsets.fromLTRB(25, 20, 25, 20)
+            ),
+            padding: EdgeInsets.all(20)
+          )
+        ]
+      )
+    );
+  }
 }
